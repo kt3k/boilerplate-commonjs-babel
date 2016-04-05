0392762fbea1007de823b990eeaae7861e983ffb
59naga <i59naga@icloud.com>
2016-04-05 18:35:02 +0900

    add rollup-plugin-json / bump abigail@1.3.5

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
-import npm from 'rollup-plugin-npm';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import pascalCase from 'pascal-case';
@@ -11,8 +12,9 @@ export default {
   format: 'umd',
   moduleName: pascalCase(require('./package.json').name),
   plugins: [
-    npm({ jsnext: true }),
+    nodeResolve({ jsnext: true }),
     commonjs(),
+    json(),
     babel(),
     uglify(),
   ],
